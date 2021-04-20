abaab37468f4ecfabbb44f2fd4879497b87c125d
Melnic Alexandru <alexandru_melnic@inbox.ru>
2021-04-20 19:22:01 +0300

    Initial commit.Added 1 main class, 1 configuration file and a Readme.

---
+++ app.java
@@ -0,0 +1,20 @@
+public class app {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b,c;
+		b=13;
+		c=2+2;
+		byte sum=(byte) 257;
+		float diff=(float) 36.6;
+        double rez=(double) a / b;
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(rez);
+	
+	} 
+	
+	
+}
